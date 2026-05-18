7cefbe97ace4c50bb1a5e2162ac2b277ce7883e3
mdsalauddin96 <salauddincse96@gmail.com>
2026-05-19 00:05:29 +0600

    connected to mongodb and create a database and pets collection

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const express=require('express')
-const cors=require('cors')
-const dotenv=require("dotenv")
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 dotenv.config();
-const app=express()
-app.use(cors())
-app.use(express.json())
-const port=process.env.PORT
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT;
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.get("/",(req,res)=>{
-    res.send("Hello from server")
-})
-
-app.listen(port,()=>{
-    console.log(`Example app listening on port ${port}`)
-})
+async function run() {
+  try {
+    await client.connect();
+    const db=client.db('pawnestDB');
+    const petsCollection=db.collection('pets');
+  
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
